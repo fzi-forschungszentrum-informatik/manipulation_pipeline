08714eacef177c6d65dcef3c28fdba869a62ba1e
Robert Wilbrandt <wilbrandt@fzi.de>
2026-02-16 18:58:58 +0100

    Add new service for splitting collision objects into multiple components

--- manipulation_pipeline/src/collision_object_manager.cpp
+++ manipulation_pipeline/src/collision_object_manager.cpp
@@ -43,6 +43,8 @@
 #include <moveit_msgs/msg/attached_collision_object.hpp>
 #include <moveit_msgs/msg/collision_object.hpp>
 #include <moveit_msgs/msg/object_color.hpp>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 namespace manipulation_pipeline {
 
@@ -52,9 +54,15 @@ CollisionObjectManager::CollisionObjectManager(
   rclcpp::Logger log)
   : m_log{std::move(log)}
   , m_planning_scene_monitor{std::move(planning_scene_monitor)}
-  , m_spawn_object_service{
-      node->create_service<SpawnObjectSrv>("~/spawn_object",
-                                           std::bind(&CollisionObjectManager::spawnObjectCb,
+  , m_spawn_object_service{node->create_service<SpawnObjectSrv>(
+      "~/spawn_object",
+      std::bind(&CollisionObjectManager::spawnObjectCb,
+                this,
+                std::placeholders::_1,
+                std::placeholders::_2))}
+  , m_split_object_service{
+      node->create_service<SplitObjectSrv>("~/split_object",
+                                           std::bind(&CollisionObjectManager::splitObjectCb,
                                                      this,
                                                      std::placeholders::_1,
                                                      std::placeholders::_2))}
@@ -154,6 +162,126 @@ void CollisionObjectManager::spawnObjectCb(std::shared_ptr<SpawnObjectSrv::Reque
   {
     response->success = false;
     response->message = fmt::format("Could not spawn object '{}': {}", request->name, ex.what());
+    RCLCPP_ERROR(m_log, "%s", response->message.c_str());
+  }
+}
+
+void CollisionObjectManager::splitObjectCb(std::shared_ptr<SplitObjectSrv::Request> request,
+                                           std::shared_ptr<SplitObjectSrv::Response> response)
+{
+  try
+  {
+    // Get current object and check if it is attached anywhere
+    const auto current_object = std::make_shared<moveit_msgs::msg::AttachedCollisionObject>();
+    {
+      planning_scene_monitor::LockedPlanningSceneRO planning_scene{m_planning_scene_monitor};
+      if (!planning_scene->getAttachedCollisionObjectMsg(*current_object, request->name))
+      {
+        if (!planning_scene->getCollisionObjectMsg(current_object->object, request->name))
+        {
+          throw std::runtime_error{
+            fmt::format("There is no collision object with name '{}'", request->name)};
+        }
+      }
+    }
+
+    const auto attach_str = current_object->link_name.empty()
+                              ? "not attached"
+                              : fmt::format("attached to {}", current_object->link_name);
+    RCLCPP_INFO(m_log,
+                "Found collision object %s at pose [%.05f, %.05f, %.05f](%.02f, %.02f, %.02f, "
+                "%.02f) in frame %s (%s)",
+                request->name.c_str(),
+                current_object->object.pose.position.x,
+                current_object->object.pose.position.y,
+                current_object->object.pose.position.z,
+                current_object->object.pose.orientation.x,
+                current_object->object.pose.orientation.y,
+                current_object->object.pose.orientation.z,
+                current_object->object.pose.orientation.w,
+                current_object->object.header.frame_id.c_str(),
+                attach_str.c_str());
+
+    response->success = true;
+    response->message = fmt::format("Successfully split object '{}' into {} components",
+                                    request->name,
+                                    request->components.size());
+
+    // Create new collision objects
+    Eigen::Isometry3d current_pose;
+    tf2::convert(current_object->object.pose, current_pose);
+
+    std::vector<std::shared_ptr<moveit_msgs::msg::CollisionObject>> collision_objects;
+    for (const auto& component : request->components)
+    {
+      Eigen::Isometry3d offset;
+      tf2::convert(component.offset, offset);
+
+      const auto component_object       = std::make_shared<moveit_msgs::msg::CollisionObject>();
+      component_object->header.frame_id = current_object->object.header.frame_id;
+      tf2::convert(current_pose * offset, component_object->pose);
+      component_object->id = component.name;
+      component_object->meshes.push_back(loadMesh(component.path));
+      component_object->subframe_names = component.subframe_names;
+      component_object->subframe_poses = component.subframe_poses;
+      component_object->operation      = moveit_msgs::msg::CollisionObject::ADD;
+
+      collision_objects.push_back(component_object);
+    }
+
+    // If object is currently attached, we first need to detach it
+    if (!current_object->link_name.empty())
+    {
+      RCLCPP_INFO(m_log, "Detaching object %s", current_object->object.id.c_str());
+      current_object->object.operation = moveit_msgs::msg::CollisionObject::REMOVE;
+      if (!m_planning_scene_monitor->processAttachedCollisionObjectMsg(current_object))
+      {
+        throw std::runtime_error{fmt::format("Unable to detach object {} from link {}",
+                                             current_object->object.id,
+                                             current_object->link_name)};
+      }
+    }
+
+    // Remove current object
+    RCLCPP_INFO(m_log, "Removing object %s from planning scene", current_object->object.id.c_str());
+    const auto collision_object_buf = std::make_shared<moveit_msgs::msg::CollisionObject>();
+    *collision_object_buf           = current_object->object;
+    if (!m_planning_scene_monitor->processCollisionObjectMsg(collision_object_buf))
+    {
+      throw std::runtime_error{
+        fmt::format("Unable to remove object {} from planning scene", collision_object_buf->id)};
+    }
+
+    // Spawn and attach new objects
+    for (const auto& component_object : collision_objects)
+    {
+      RCLCPP_INFO(m_log, "Spawning collision object %s", component_object->id.c_str());
+      if (!m_planning_scene_monitor->processCollisionObjectMsg(component_object))
+      {
+        throw std::runtime_error{
+          fmt::format("Unable to spawn collision object for component {}", component_object->id)};
+      }
+
+      if (!current_object->link_name.empty())
+      {
+        RCLCPP_INFO(m_log,
+                    "Attaching object %s to %s",
+                    component_object->id.c_str(),
+                    current_object->link_name.c_str());
+        current_object->object = *component_object;
+        if (!m_planning_scene_monitor->processAttachedCollisionObjectMsg(current_object))
+        {
+          throw std::runtime_error{fmt::format("Unable to attach new collision object {} to {}",
+                                               current_object->object.id,
+                                               current_object->link_name)};
+        }
+      }
+    }
+  }
+  catch (const std::runtime_error& ex)
+  {
+    response->success = false;
+    response->message = fmt::format("Could not split object '{}': {}", request->name, ex.what());
     RCLCPP_ERROR(m_log, "%s", response->message.c_str());
   }
 }
